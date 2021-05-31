ff0e90babbe76a4aa21764bd63682526b81b5f30
Pinxuan Huang <pinxhuang@gmail.com>
2021-05-31 14:55:06 -0400

    Add news section

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,45 @@
+// ===================== NEWS TEMPLATE =====================
+const newsData = [
+  { 
+    title: 'Soul Guard Released!',
+    date: 'April 30, 2021',
+    summary: 'Soul Guard, an action adventure 3D game, is now available on itch.io!',
+    image: 'https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png'
+  },
+  { 
+    title: 'Post Title',
+    date: 'Month DD, 20XX',
+    summary: 'Post summary...',
+    image: 'https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png'
+  },
+];
+
+function addNews(templateId) {
+  const newsList = document.getElementById('news');
+  const fragment = document.getElementById(templateId);
+  
+  // Clear out content
+  newsList.innerHTML = '';
+  
+  // Loop over game data and modify the given template
+  newsData.forEach(post => {
+    // Create an instance of the template content
+    const instance = document.importNode(fragment.content, true);
+
+    // Add relevant content to the template
+    instance.querySelector('.post-date').innerHTML = post.date;
+    instance.querySelector('.post-title').innerHTML = post.title;
+    instance.querySelector('.post-summary').innerHTML = post.summary;
+    instance.querySelector('.post-thumbnail').src = post.image;
+
+    // Append the instance ot the DOM
+    newsList.appendChild(instance);
+  });  
+}
+
+addNews('news-template');
+
+// ===================== GAMES TEMPLATE =====================
 const gameData = [
   { 
     title: 'Soul Guard', 
