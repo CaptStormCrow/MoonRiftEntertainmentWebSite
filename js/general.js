db4c06e92971f4ffe7f5011727f77bc4b8c74b68
Pinxuan Huang <pinxhuang@gmail.com>
2021-07-07 19:08:17 -0400

    Load footer as component

--- js/general.js
+++ js/general.js
@@ -3,6 +3,48 @@
   such as the navigation bar and footer.
  */
 
+// ===================== INCLUDE HTML COMPONENTS =====================
+// Source: https://www.w3schools.com/howto/howto_html_include.asp
+
+function includeHTML() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log("start includeHTML");
+      var z, i, elmnt, file, xhttp;
+      /*loop through a collection of all HTML elements:*/
+      z = document.getElementsByTagName("*");
+      for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+          /*make an HTTP request using the attribute value as the file name:*/
+          xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+              if (this.status == 200) {
+                elmnt.innerHTML = this.responseText;
+              }
+              if (this.status == 404) {
+                elmnt.innerHTML = "Page not found.";
+              }
+              /*remove the attribute, and call this function once more:*/
+              elmnt.removeAttribute("include-html");
+              includeHTML();
+            }
+          };
+          xhttp.open("GET", file, true);
+          xhttp.send();
+          /*exit the function:*/
+          return;
+        }
+      }
+      console.log("end includeHTML");
+      resolve();
+    }, 250);
+  });
+}
+
 // =============== SIDENAV ===============
 var isSidenavOpen = false;
 
@@ -20,43 +62,51 @@ function openSidenav() {
 
 function closeSidenav() {
   document.getElementById("sidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.marginLeft = "0";
   isSidenavOpen = false;
 }
 
 // =============== FOOTER SOCIAL MEDIA ICONS TEMPLATE ===============
-const relPath = './assets/icons/social-media/';
+const relPath = "../assets/icons/social-media/";
 const iconData = [
   {
-    link: 'https://www.facebook.com/MoonRiftEntertainment/',
-    imagePath: `${relPath}036-facebook-silver.svg`
+    link: "https://www.facebook.com/MoonRiftEntertainment/",
+    imagePath: `${relPath}036-facebook-silver.svg`,
   },
   {
-    link: 'https://twitter.com/MoonRiftLLC/',
-    imagePath: `${relPath}008-twitter-silver.svg`
+    link: "https://twitter.com/MoonRiftLLC/",
+    imagePath: `${relPath}008-twitter-silver.svg`,
   },
   {
-    link: 'https://www.youtube.com/channel/UCECOpIunnDsx0vtFu5Uqaow',
-    imagePath: `${relPath}001-youtube-silver.svg`
+    link: "https://www.youtube.com/channel/UCECOpIunnDsx0vtFu5Uqaow",
+    imagePath: `${relPath}001-youtube-silver.svg`,
   },
   {
-    link: 'https://www.instagram.com/MoonRiftEntertainment/',
-    imagePath: `${relPath}029-instagram-silver.svg`
+    link: "https://www.instagram.com/MoonRiftEntertainment/",
+    imagePath: `${relPath}029-instagram-silver.svg`,
   },
 ];
 
-function addSocials(templateId) {
-  const socialsList = document.getElementById('footer-socials');
-  const fragment = document.getElementById(templateId);
-  socialsList.innerHTML = '';
-  iconData.forEach(icon => {
-    const instance = document.importNode(fragment.content, true);
+function addFooterSocials(templateId) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log("start addFooterSocials");
+      const socialsList = document.getElementById("footer-socials");
+      const fragment = document.getElementById(templateId);
+      socialsList.innerHTML = "";
+      iconData.forEach((icon) => {
+        const instance = document.importNode(fragment.content, true);
 
-    instance.querySelector('.footer-social-link').href = icon.link;
-    instance.querySelector('.footer-social-icon').src = icon.imagePath;
-    
-    socialsList.appendChild(instance);
-  });  
+        instance.querySelector(".footer-social-link").href = icon.link;
+        instance.querySelector(".footer-social-icon").src = icon.imagePath;
+
+        socialsList.appendChild(instance);
+      });
+      console.log("end addFooterSocials");
+      resolve();
+    }, 500);
+  });
 }
 
-addSocials('footer-social-template');
+// Load footer
+includeHTML().then(addFooterSocials("footer-social-template"));
