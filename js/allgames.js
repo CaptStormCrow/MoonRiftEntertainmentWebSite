28c1bb36e2415a8272adcdd8a8ae7ccae01ba747
Pinxuan Huang <pinxhuang@gmail.com>
2021-07-08 12:39:46 -0400

    Populate content for all game pages

--- js/allgames.js
+++ js/allgames.js
@@ -2,86 +2,59 @@
 
 // data for all games, modify as needed
 const gameData = [
-  { 
-    title: 'Kiwi', 
-    genres: 'Arcade',
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzYxNTY2MDgucG5n/315x250%23c/ay6JFm.png', 
-    link: 'https://moonrift.itch.io/kiwi' 
+  {
+    title: "Kiwi",
+    genres: "Arcade",
+    price: "FREE TO PLAY",
+    image: "https://img.itch.zone/aW1nLzYxNTY2MDgucG5n/315x250%23c/ay6JFm.png",
+    link: "./pages/game_pages/kiwi.html",
   },
-  { 
-    title: 'Soul Guard', 
-    genres: 'Action, Adventure, 3D',
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png', 
-    link: 'https://moonrift.itch.io/soul-guard' 
+  {
+    title: "Soul Guard",
+    genres: "Action, Adventure, 3D",
+    price: "FREE TO PLAY",
+    image:
+      "https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png",
+    link: "./pages/game_pages/soulguard.html",
   },
-  { 
-    title: 'Toy Kingdom 2', 
-    genres: 'Sandbox, 3D', 
-    price: '$4 USD', 
-    image: 'https://img.itch.zone/aW1nLzU0NTk5MTAucG5n/315x250%23c/BqPfbN.png', 
-    link: 'https://moonrift.itch.io/toy-kingdom-2' 
+  {
+    title: "Toy Kingdom 2",
+    genres: "Sandbox, 3D",
+    price: "$4 USD",
+    image: "https://img.itch.zone/aW1nLzU0NTk5MTAucG5n/315x250%23c/BqPfbN.png",
+    link: "./pages/game_pages/tk2.html",
   },
-  { 
-    title: 'Toy Kingdom', 
-    genres: 'Sandbox, 3D', 
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzUxODcxNjUucG5n/315x250%23c/IcKa2U.png',
-    link: 'https://moonrift.itch.io/toy-kingdom' 
+  {
+    title: "Toy Kingdom",
+    genres: "Sandbox, 3D",
+    price: "FREE TO PLAY",
+    image: "https://img.itch.zone/aW1nLzUxODcxNjUucG5n/315x250%23c/IcKa2U.png",
+    link: "./pages/game_pages/tk1.html",
   },
-  { 
-    title: 'Kiwi', 
-    genres: 'Arcade',
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzYxNTY2MDgucG5n/315x250%23c/ay6JFm.png', 
-    link: 'https://moonrift.itch.io/kiwi' 
-  },
-  { 
-    title: 'Soul Guard', 
-    genres: 'Action, Adventure, 3D',
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png', 
-    link: 'https://moonrift.itch.io/soul-guard' 
-  },
-  { 
-    title: 'Toy Kingdom 2', 
-    genres: 'Sandbox, 3D', 
-    price: '$4 USD', 
-    image: 'https://img.itch.zone/aW1nLzU0NTk5MTAucG5n/315x250%23c/BqPfbN.png', 
-    link: 'https://moonrift.itch.io/toy-kingdom-2' 
-  },
-  { 
-    title: 'Toy Kingdom', 
-    genres: 'Sandbox, 3D', 
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzUxODcxNjUucG5n/315x250%23c/IcKa2U.png',
-    link: 'https://moonrift.itch.io/toy-kingdom' 
-  }
 ];
 
 function addGames(templateId) {
-  const gamesList = document.getElementById('games');
+  const gamesList = document.getElementById("games");
   const fragment = document.getElementById(templateId);
-  
+
   // Clear out content
-  gamesList.innerHTML = '';
-  
+  gamesList.innerHTML = "";
+
   // Loop over game data and modify the given template
-  gameData.forEach(game => {
+  gameData.forEach((game) => {
     // Create an instance of the template content
     const instance = document.importNode(fragment.content, true);
 
     // Add relevant content to the template
-    instance.querySelector('.game-item').href = game.link;
-    instance.querySelector('.game-card-title').innerHTML = game.title;
-    instance.querySelector('.genres').innerHTML = game.genres;
-    instance.querySelector('.price').innerHTML = game.price;
-    instance.querySelector('.thumbnail').src = game.image;
+    instance.querySelector(".game-item").href = game.link;
+    instance.querySelector(".game-card-title").innerHTML = game.title;
+    instance.querySelector(".genres").innerHTML = game.genres;
+    instance.querySelector(".price").innerHTML = game.price;
+    instance.querySelector(".thumbnail").src = game.image;
 
     // Append the instance ot the DOM
     gamesList.appendChild(instance);
-  });  
+  });
 }
 
-addGames('game-template');
+addGames("game-template");
--- js/homepage.js
+++ js/homepage.js
@@ -2,18 +2,20 @@
 // Might need text or other data so featuredData is an array of objects
 const featuredData = [
   {
-    image: 'https://moonrift.files.wordpress.com/2021/04/soulguardofficial.png'
+    image: "https://moonrift.files.wordpress.com/2021/04/soulguardofficial.png",
   },
   {
-    image: 'https://moonrift.files.wordpress.com/2021/03/resized-toy-kingdom-2-1.png?w=1024'
+    image:
+      "https://moonrift.files.wordpress.com/2021/03/resized-toy-kingdom-2-1.png?w=1024",
   },
   {
-    image: 'https://moonrift.files.wordpress.com/2021/03/raptornest.png?w=1024'
+    image: "https://moonrift.files.wordpress.com/2021/03/raptornest.png?w=1024",
   },
   {
-    image: 'https://moonrift.files.wordpress.com/2021/03/lightningstone.png?w=840'
-  }
-]
+    image:
+      "https://moonrift.files.wordpress.com/2021/03/lightningstone.png?w=840",
+  },
+];
 
 var timer;
 var slideId = 0;
@@ -29,7 +31,7 @@ function nextSlide() {
 
 function setCarouselImage(id) {
   // Change background image to slide id
-  let featuredImage = document.getElementById('featured-img');
+  let featuredImage = document.getElementById("featured-img");
   changeBg(featuredImage, featuredData[id].image);
   // Go to next slide every set interval
   clearTimeout(timer);
@@ -38,107 +40,111 @@ function setCarouselImage(id) {
 
 // Change background of element 'el' to 'imgPath'
 function changeBg(el, imgPath) {
-  let imgString = 'url(' + imgPath + ')';
-  el.style.background =  imgString;
+  let imgString = "url(" + imgPath + ")";
+  el.style.background = imgString;
   el.style.backgroundPosition = "center";
   el.style.backgroundRepeat = "no-repeat";
 }
 
 // ===================== NEWS TEMPLATE =====================
 const newsData = [
-  { 
-    title: 'Soul Guard Released!',
-    date: 'April 30, 2021',
-    summary: 'Soul Guard, an action adventure 3D game, is now available on itch.io!',
-    image: 'https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png'
+  {
+    title: "Soul Guard Released!",
+    date: "April 30, 2021",
+    summary:
+      "Soul Guard, an action adventure 3D game, is now available on itch.io!",
+    image:
+      "https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png",
   },
-  { 
-    title: 'Post Title',
-    date: 'Month DD, 20XX',
-    summary: 'Post summary...',
-    image: 'https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png'
+  {
+    title: "Post Title",
+    date: "Month DD, 20XX",
+    summary: "Post summary...",
+    image:
+      "https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png",
   },
 ];
 
 function addNews(templateId) {
-  const newsList = document.getElementById('news');
+  const newsList = document.getElementById("news");
   const fragment = document.getElementById(templateId);
-  
+
   // Clear out content
-  newsList.innerHTML = '';
-  
+  newsList.innerHTML = "";
+
   // Loop over game data and modify the given template
-  newsData.forEach(post => {
+  newsData.forEach((post) => {
     // Create an instance of the template content
     const instance = document.importNode(fragment.content, true);
 
     // Add relevant content to the template
-    instance.querySelector('.post-date').innerHTML = post.date;
-    instance.querySelector('.post-title').innerHTML = post.title;
-    instance.querySelector('.post-summary').innerHTML = post.summary;
-    instance.querySelector('.post-thumbnail').src = post.image;
+    instance.querySelector(".post-date").innerHTML = post.date;
+    instance.querySelector(".post-title").innerHTML = post.title;
+    instance.querySelector(".post-summary").innerHTML = post.summary;
+    instance.querySelector(".post-thumbnail").src = post.image;
 
     // Append the instance ot the DOM
     newsList.appendChild(instance);
-  });  
+  });
 }
 
-addNews('news-template');
+addNews("news-template");
 
 // ===================== GAMES TEMPLATE =====================
 const gameData = [
-  { 
-    title: 'Kiwi', 
-    genres: 'Arcade',
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzYxNTY2MDgucG5n/315x250%23c/ay6JFm.png', 
-    link: './pages/game_pages/kiwi.html' 
+  {
+    title: "Kiwi",
+    genres: "Arcade",
+    price: "FREE TO PLAY",
+    image: "https://img.itch.zone/aW1nLzYxNTY2MDgucG5n/315x250%23c/ay6JFm.png",
+    link: "./pages/game_pages/kiwi.html",
   },
-  { 
-    title: 'Soul Guard', 
-    genres: 'Action, Adventure, 3D',
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png', 
-    link: 'https://moonrift.itch.io/soul-guard' 
+  {
+    title: "Soul Guard",
+    genres: "Action, Adventure, 3D",
+    price: "FREE TO PLAY",
+    image:
+      "https://img.itch.zone/aW1nLzU4MjAyNDIucG5n/315x250%23c/5pGN%2FO.png",
+    link: "./pages/game_pages/soulguard.html",
   },
-  { 
-    title: 'Toy Kingdom 2', 
-    genres: 'Sandbox, 3D', 
-    price: '$4 USD', 
-    image: 'https://img.itch.zone/aW1nLzU0NTk5MTAucG5n/315x250%23c/BqPfbN.png', 
-    link: 'https://moonrift.itch.io/toy-kingdom-2' 
+  {
+    title: "Toy Kingdom 2",
+    genres: "Sandbox, 3D",
+    price: "$4 USD",
+    image: "https://img.itch.zone/aW1nLzU0NTk5MTAucG5n/315x250%23c/BqPfbN.png",
+    link: "./pages/game_pages/tk2.html",
   },
-  { 
-    title: 'Toy Kingdom', 
-    genres: 'Sandbox, 3D', 
-    price: 'FREE TO PLAY', 
-    image: 'https://img.itch.zone/aW1nLzUxODcxNjUucG5n/315x250%23c/IcKa2U.png',
-    link: 'https://moonrift.itch.io/toy-kingdom' 
+  {
+    title: "Toy Kingdom",
+    genres: "Sandbox, 3D",
+    price: "FREE TO PLAY",
+    image: "https://img.itch.zone/aW1nLzUxODcxNjUucG5n/315x250%23c/IcKa2U.png",
+    link: "./pages/game_pages/tk1.html",
   },
 ];
 
 function addGames(templateId) {
-  const gamesList = document.getElementById('games');
+  const gamesList = document.getElementById("games");
   const fragment = document.getElementById(templateId);
-  
+
   // Clear out content
-  gamesList.innerHTML = '';
-  
+  gamesList.innerHTML = "";
+
   // Loop over game data and modify the given template
-  gameData.forEach(game => {
+  gameData.forEach((game) => {
     // Create an instance of the template content
     const instance = document.importNode(fragment.content, true);
 
     // Add relevant content to the template
-    instance.querySelector('.game-item').href = game.link;
-    instance.querySelector('.game-card-title').innerHTML = game.title;
-    instance.querySelector('.genres').innerHTML = game.genres;
-    instance.querySelector('.price').innerHTML = game.price;
-    instance.querySelector('.thumbnail').src = game.image;
+    instance.querySelector(".game-item").href = game.link;
+    instance.querySelector(".game-card-title").innerHTML = game.title;
+    instance.querySelector(".genres").innerHTML = game.genres;
+    instance.querySelector(".price").innerHTML = game.price;
+    instance.querySelector(".thumbnail").src = game.image;
 
     // Append the instance ot the DOM
     gamesList.appendChild(instance);
-  });  
+  });
 }
 
-addGames('game-template');
+addGames("game-template");
