5c8a484fa7a1ccafb7289343f51f2263738eeea5
Pinxuan Huang <pinxhuang@gmail.com>
2021-07-07 20:42:43 -0400

    Remove method that dynamically renders footer social icons

--- js/general.js
+++ js/general.js
@@ -7,41 +7,39 @@
 // Source: https://www.w3schools.com/howto/howto_html_include.asp
 
 function includeHTML() {
-  return new Promise(function (resolve) {
-    console.log("start includeHTML");
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("include-html");
-      if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-          if (this.readyState == 4) {
-            if (this.status == 200) {
-              elmnt.innerHTML = this.responseText;
-            }
-            if (this.status == 404) {
-              elmnt.innerHTML = "Page not found.";
-            }
-            /*remove the attribute, and call this function once more:*/
-            elmnt.removeAttribute("include-html");
-            includeHTML();
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
           }
-        };
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /*exit the function:*/
-        return;
-      }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
     }
-    console.log("end includeHTML");
-    resolve();
-  });
+  }
 }
+
+// Load footer
+includeHTML(); //.then(addFooterSocials("footer-social-template"));
 
 // =============== SIDENAV ===============
 var isSidenavOpen = false;
@@ -63,46 +61,3 @@ function closeSidenav() {
   document.getElementById("main").style.marginLeft = "0";
   isSidenavOpen = false;
 }
-
-// =============== FOOTER SOCIAL MEDIA ICONS TEMPLATE ===============
-const relPath = "../assets/icons/social-media/";
-const iconData = [
-  {
-    link: "https://www.facebook.com/MoonRiftEntertainment/",
-    imagePath: `${relPath}036-facebook-silver.svg`,
-  },
-  {
-    link: "https://twitter.com/MoonRiftLLC/",
-    imagePath: `${relPath}008-twitter-silver.svg`,
-  },
-  {
-    link: "https://www.youtube.com/channel/UCECOpIunnDsx0vtFu5Uqaow",
-    imagePath: `${relPath}001-youtube-silver.svg`,
-  },
-  {
-    link: "https://www.instagram.com/MoonRiftEntertainment/",
-    imagePath: `${relPath}029-instagram-silver.svg`,
-  },
-];
-
-function addFooterSocials(templateId) {
-  return new Promise(function (resolve) {
-    console.log("start addFooterSocials");
-    const socialsList = document.getElementById("footer-socials");
-    const fragment = document.getElementById(templateId);
-    socialsList.innerHTML = "";
-    iconData.forEach((icon) => {
-      const instance = document.importNode(fragment.content, true);
-
-      instance.querySelector(".footer-social-link").href = icon.link;
-      instance.querySelector(".footer-social-icon").src = icon.imagePath;
-
-      socialsList.appendChild(instance);
-    });
-    console.log("end addFooterSocials");
-    resolve();
-  });
-}
-
-// Load footer
-includeHTML(); //.then(addFooterSocials("footer-social-template"));
